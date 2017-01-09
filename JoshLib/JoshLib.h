4949572e8f1e539db2f335a8ddd7dacfc6dc67c0
Josh Baxley <josh.baxley@gmail.com>
2017-01-09 12:51:37 -0800

    Initial Commit

---
+++ JoshLib/JoshLib.h
@@ -0,0 +1,19 @@
+//
+//  JoshLib.h
+//  JoshLib
+//
+//  Created by Josh Baxley on 1/9/17.
+//  Copyright © 2017 Josh Baxley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JoshLib.
+FOUNDATION_EXPORT double JoshLibVersionNumber;
+
+//! Project version string for JoshLib.
+FOUNDATION_EXPORT const unsigned char JoshLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JoshLib/PublicHeader.h>
+
+
